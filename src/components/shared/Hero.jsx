6fbaf0fa66405dc17669ff9fdf62750229bf6dbb
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-05-10 12:10:59 +0200

    change hero image to never stop learning

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -4,7 +4,7 @@ import "../../styles/Hero.css";
 export default function Hero() {
 	return (
 		<section className="hero">
-			<img className="hero-image" src="/images/love-to-learn.jpg" alt="hero" />
+			<img className="hero-image" src="/images/hero.jpeg" alt="hero" />
 			<div className="hero-text">
 				<h1 className="hero-title">Mind Match</h1>
 				<img src="/images/logo.png" alt="logo" />
