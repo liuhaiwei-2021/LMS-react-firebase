fb2053f298f52d7684f24de1e30aebeeff7e40d2
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-06-18 08:26:41 +0200

    add illstration picture on home page and add box-shadow for navigation

--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
 			<img className="hero-image" src="" alt="hero" />
 			<div className="hero-text">
 				<h1 className="hero-title">Mind Match</h1>
-				<img src="/images/logo.png" alt="logo" />
+				<img src="/images/person-studying-online.png" alt="logo" />
 				<h3 className="slogan">Unlock the power of you</h3>
 				<h4>Get job-ready for an in-demand career </h4>
 				<p>Break into a new field </p>
