ac5119d15c6344c0acacf71f8a44ba08f4db808f
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-12-08 10:13:32 +0100

    delete hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
-//Project file
-import Hero from "../components/shared/Hero";
+import "../styles/Hero.css";
 
 export default function Home() {
 	return (
 		<div className="home">
-			<Hero />
+			<section className="hero">
+				<img className="hero-image" src="" alt="hero" />
+				<div className="hero-text">
+					<h1 className="hero-title">Mind Match</h1>
+					<img src="/images/person-studying-online.png" alt="logo" />
+					<h3 className="sloga n">Unlock the power of you</h3>
+					<h4>Get job-ready for an in-demand career </h4>
+					<p>Break into a new field </p>
+				</div>
+			</section>
 		</div>
 	);
 }
